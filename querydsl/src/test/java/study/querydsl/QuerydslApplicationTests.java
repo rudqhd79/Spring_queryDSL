a02330121ab85dd4a4d8785e623224b9118da3e0
rudqhd79 <rudqhd79@naver.com>
2023-07-11 18:34:30 +0900

    20230711 3

--- querydsl/src/test/java/study/querydsl/QuerydslApplicationTests.java
+++ querydsl/src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -1,6 +1,7 @@
 package study.querydsl;
 
 import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -9,24 +10,29 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
+import org.springframework.test.annotation.Commit;
+import study.querydsl.Entity.QTestEntity;
 import study.querydsl.Entity.TestEntity;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@Transactional
+@Commit
 class QuerydslApplicationTests {
 
 	@Autowired
 	EntityManager em;
-
+/*
 	@Test
 	void contextLoads() {
-		TestEntity hello = new TestEntity();
-		em.persist(hello);
+		TestEntity testEntity = new TestEntity();
+		em.persist(testEntity);
 		JPAQueryFactory query = new JPAQueryFactory(em);
-		QTestEntity QHello = QHello.hello; // Querydsl Q타입 동작 확인
-		TestEntity result = query.selectFrom(QHello).fetchOne();
-		Assertions.assertThat(result).isEqualTo(hello);
+		QTestEntity QHello = QTestEntity.testEntity; // Querydsl Q타입 동작 확인
+		TestEntity result = (TestEntity) query.selectFrom(QHello).fetchOne();
+		assertThat(result).isEqualTo(testEntity);
 		// lombok 동작 확인 (hello.getId())
-		Assertions.assertThat(result.getId()).isEqualTo(hello.getId());
+		assertThat(result.getId()).isEqualTo(testEntity.getId());
 	}
-
+*/
 }
